9a60c4bba5f488df6f48ea42542896b3d2a54d7e
Alain Xu <agent.xenial@protonmail.com>
2024-09-15 10:47:43 -0400

    UPDATE 7: Art and Code
    - Added the sketch created by Rudy Jarvis
    - Modified the code with safety checks to prevent unexpected behaviour

--- iteration-1/nullptr_logo.gd
+++ iteration-1/nullptr_logo.gd
@@ -1,19 +1,30 @@
 extends Sprite2D
 
-var timeCounter = 0
+var alphaValue:float = 0.0
+var timeCounter:float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	modulate.a = 0
+	modulate.a = alphaValue
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (modulate.a < 1 and timeCounter < 1):
-		modulate.a += (1.0/2.0)*delta
+	if (alphaValue < 1 and timeCounter < 1):
+		alphaValue += (1.0/2.0)*delta
+		if (alphaValue > 1):
+			alphaValue = 1
 	else:
 		if (timeCounter < 1):
 			timeCounter += 1.0*delta
 		else:
-			if (modulate.a > 0):
-				modulate.a -= (1.0/2.0)*delta
+			if (alphaValue > 0):
+				alphaValue -= (1.0/2.0)*delta
+				if (alphaValue < 0):
+					alphaValue = 0
+	
+	modulate.a = alphaValue
+	
+	# DEBUG
+	# print(modulate.a)
+	# print(timeCounter)
