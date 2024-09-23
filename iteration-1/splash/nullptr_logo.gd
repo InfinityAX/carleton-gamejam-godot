4f3eb21350da8faba475a14db02db17f93a2e6cc
Alain Xu <agent.xenial@protonmail.com>
2024-09-23 16:22:24 -0400

    UPDATE 18: Code
    - Added the code to go from the splash scene to the main menu scene

--- iteration-1/splash/nullptr_logo.gd
+++ iteration-1/splash/nullptr_logo.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 var alphaValue:float = 0.0
 var timeCounter:float = 0.0
+var splashShowDone = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,20 +11,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (alphaValue < 1 and timeCounter < 1):
-		alphaValue += (1.0/2.0)*delta
-		if (alphaValue > 1):
-			alphaValue = 1
-	else:
-		if (timeCounter < 1):
-			timeCounter += 1.0*delta
+	if (splashShowDone):
+		if (timeCounter > 2):
+			get_tree().change_scene_to_file("res://mainMenu/sceneMainMenu.tscn")
 		else:
-			if (alphaValue > 0):
-				alphaValue -= (1.0/2.0)*delta
-				if (alphaValue < 0):
-					alphaValue = 0
-	
-	modulate.a = alphaValue
+			timeCounter += 1.0*delta
+	else:
+		if (alphaValue < 1 and timeCounter < 1):
+			alphaValue += (1.0/2.0)*delta
+			if (alphaValue > 1):
+				alphaValue = 1
+		else:
+			if (timeCounter < 1):
+				timeCounter += 1.0*delta
+			else:
+				if (alphaValue > 0):
+					alphaValue -= (1.0/2.0)*delta
+					if (alphaValue < 0):
+						alphaValue = 0
+						splashShowDone = true
+		
+		modulate.a = alphaValue
 	
 	# DEBUG
 	# print(modulate.a)
